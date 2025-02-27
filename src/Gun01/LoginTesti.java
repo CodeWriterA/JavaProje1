be99d7a8d7d99410bc8e08ce540d2a45154bc8a1
erdem <erdemmozkann@hotmail.com>
2025-02-27 20:24:18 +0300

    Login testi Aşama 1 (jirada size gelen taskdaki başlık)

---
+++ src/Gun01/LoginTesti.java
@@ -0,0 +1,16 @@
+package Gun01;
+
+public class LoginTesti {
+    public static void main(String[] args) {
+
+        //Bu bölüm Login testi için, yeni şifre modeline göre yazıldı.
+        System.out.println("merhaba Dünya");
+
+        // çok satır yazdım ve kodları bitirdim
+        System.out.println("Login testi 1 ");
+
+
+
+
+    }
+}
