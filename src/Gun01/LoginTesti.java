ffe8ea047684410980140171f57182a0ea1dd884
erdem <erdemmozkann@hotmail.com>
2025-02-27 20:33:39 +0300

    yeni Login Testi yapıldı

--- src/Gun01/LoginTesti.java
+++ src/Gun01/LoginTesti.java
@@ -4,12 +4,12 @@ public class LoginTesti {
     public static void main(String[] args) {
 
         //Bu bölüm Login testi için, yeni şifre modeline göre yazıldı.
-        System.out.println("merhaba Dünya");
 
-        // çok satır yazdım ve kodları bitirdim
-        System.out.println("Login testi 1 ");
+        System.out.println("Yeni Login testi 1 ");
 
         System.out.println("Sepet testi yazıldı bitirildi, 3 günlük kod yazıldı");
+
+        System.out.println("Yeni Login Testi yapıldı, 7 gün uğraştım");
 
 
 
